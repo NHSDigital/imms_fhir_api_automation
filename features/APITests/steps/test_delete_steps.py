4cc6cf1971053d8a89a6a807a0b8b95f903ef364
Krupasindhu Baral <krupasindhu.baral@nhs.net>
2025-11-24 16:40:47 +0000

    config update batch  fixes

--- features/APITests/steps/test_delete_steps.py
+++ features/APITests/steps/test_delete_steps.py
@@ -30,8 +30,7 @@ def send_delete_for_immunization_event_by_supplier(context, Supplier):
 @then('The delta table will be populated with the correct data for deleted event')
 def validate_imms_delta_table_by_ImmsID(context):
     create_obj = context.create_object
-    expected_version = 2
-    items = fetch_immunization_int_delta_detail_by_immsID(context.aws_profile_name, context.ImmsID, context.S3_env, expected_version)
+    items = fetch_immunization_int_delta_detail_by_immsID(context.aws_profile_name, context.ImmsID, context.S3_env, 2)
     assert items, f"Items not found in response for ImmsID: {context.ImmsID}"
 
     # Find the latest item where operation is DELETE
